7ac03ca7c1d61fba2bd82f4dc25e8c345303cf20
evdy13 <evdy13@users.noreply.github.com>
2014-10-12 18:20:15 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,3 +1,8 @@
+## create a histogram of Global_active_power
+## add axis labels and color
+
 hist(hpc$Global_active_power, xlab="Global Active Power (kilowatts)", main="Global Active Power" , col="red")
+
+## create a png of the plot
 dev.copy(png, file = "plot1.png")
 dev.off()
