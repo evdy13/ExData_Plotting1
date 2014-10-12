0aa19457e54f84aebd22e6effab70fff0eb9e6f3
evdy13 <evdy13@users.noreply.github.com>
2014-10-12 18:24:00 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,4 +1,12 @@
+## create a Time_Date merged column
+hpc$Time_Date <- strptime(paste(hpc$Date, hpc$Time, sep=","), format="%d/%m/%Y,%H:%M:%S")
+
+## create plot, make it blank intially, assign axis labels
 plot(hpc$Time_Date, hpc$Global_active_power, type = "n", xlab = "", ylab="Global Active Power (kilowatts)")
+
+## fill in lines
 lines(hpc$Time_Date, hpc$Global_active_power, type ="l")
+
+## create png of graph
 dev.copy(png, file = "plot2.png")
 dev.off()
